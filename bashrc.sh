0c0cecd707daa5863e1196269bbf2b16f5d804b4
Bob Geis <rgeis@athenahealth.com>
2020-12-04 10:26:55 -0500

    git: amend glon

--- bashrc.sh
+++ bashrc.sh
@@ -235,25 +235,22 @@ alias gl="git log"
 alias glo="git log --oneline -n 10"
 alias gloo="git log --oneline -n 20"
 alias glooo="git log --oneline"
-function git-log-oneline-numbered() {
-  local lines_total
-  local lines_left
-  local chunk_size
-  chunk_size=4
-  lines_total="$1"
-  if [ -z "$lines_total" ]; then
-    lines_total=$(( $chunk_size * 2 ))
-  fi
-  lines_left="$lines_total"
-  while [ "$lines_left" -gt "$chunk_size" ]
+function git-log-oneline-chunked() {
+  local chunks
+  local chunk
+  local size
+  chunks="${1:-2}"
+  size="${2:-4}"
+  chunk=0
+  while [ "$chunk" -lt "$chunks" ]
   do
-    git log --oneline -n "$chunk_size" --skip=$(( $lines_total-$lines_left ))
-    lines_left=$(( $lines_left-$chunk_size ))
-    echo ""
+    echo $(( $chunk*$size))"-"$(( $chunk*$size+$size ))":"
+    git log --oneline -n "$size" --skip=$(( $chunk*$size ))
+    chunk=$(( $chunk+1 ))
   done
-  git log --oneline -n "$lines_left" --skip=$(( $lines_total-$lines_left ))
 }
-alias glon="git-log-oneline-numbered"
+alias glon="git-log-oneline-chunked"
+alias glonn="git-log-oneline-chunked 4"
 alias gll="git log -3 HEAD"
 alias glll="git log -6 HEAD"
 alias glos="git log --stat --oneline -n 5"

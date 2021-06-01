680d3551eb3bdda2f1a50a4b0c31c3ff40e665f9
Bob Geis <rgeis@athenahealth.com>
2021-06-01 19:03:45 -0400

    bash: update mvn-cir, add mvn-cit

--- bashrc.sh
+++ bashrc.sh
@@ -426,7 +426,8 @@ alias mvn-run="mvn spring-boot:run"
 alias mvn-runD='mvn spring-boot:run -Drun.arguments="-Djavax.net.ssl.trustStore=cacerts"'
 alias mvn-cleanrun="mvn clean && mvn spring-boot:run"
 alias mvn-ci="mvn clean && mvn clean install"
-alias mvn-cir="mvn clean && mvn clean install && mvn-runD"
+alias mvn-cir="mvn clean && mvn clean install && mvn-run"
+alias mvn-cit="mvn clean && mvn clean install && bin/test.sh"
 alias mvn-clear-cache="rm -rf ~/.m2/repository"
 
 ########

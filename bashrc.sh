da69c7e782e8a271611a51bae5b730f0f411e729
Bob Geis <rgeis@athenahealth.com>
2021-08-23 18:00:09 -0400

    bash: add make-exec

--- bashrc.sh
+++ bashrc.sh
@@ -474,6 +474,7 @@ alias c="reset"
 alias sound="afplay /System/Library/Sounds/Submarine.aiff"
 alias blowsound="afplay /System/Library/Sounds/Blow.aiff"
 alias bb="blowsound && blowsound"
+alias make-exec="chmod 755 " # follow with filename to make it executable
 alias sudo="sudo " # allow aliases to be sudo'ed
 alias echo-path="alias path='echo -e ${PATH//:/\\n}'"
 alias map="xargs -n1" # map 'function', see https://github.com/mathiasbynens/dotfiles/blob/main/.aliases
